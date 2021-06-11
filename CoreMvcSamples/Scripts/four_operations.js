54ab7865201e8989fc255c294baa08a3b9a8aa60
yihu wang <wangyihu2009@msn.com>
2021-06-11 15:42:52 +0800

    #

--- CoreMvcSamples/Scripts/four_operations.js
+++ CoreMvcSamples/Scripts/four_operations.js
@@ -1,16 +1,16 @@
-var add = function (callback, x, y) {
+var add = function(callback, x, y) {
     var result = x + y;
     callback(null, result);
 };
-var subtract = function (callback, x, y) {
+var subtract = function(callback, x, y) {
     var result = x - y;
     callback(null, result);
 };
-var multiplicati = function (callback, x, y) {
+var multiplicati = function(callback, x, y) {
     var result = x * y;
     callback(null, result);
 };
-var division = function (callback, x, y) {
+var division = function(callback, x, y) {
     var result = NaN;
     if (y != 0) {
         result = x / y;
